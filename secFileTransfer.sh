56332f64fb4eb4e4da2201ad4727dc2ce7aab708
4urb4nm0nk <jonasmelo@edu.unisinos.br>
2021-03-15 14:23:55 -0300

    v1.1

--- secFileTransfer.sh
+++ secFileTransfer.sh
@@ -4,24 +4,30 @@
 apt install veracrypt magic-wormhole -y
 
 # Cria o container
-veracrypt -t -c ./secureFileTransfer
+read -s -p "Defina a senha do container Veracrypt: " senhaContainer
+veracrypt -t -c secureFileTransfer --volume-type=normal --encryption=aes --hash=sha-512 --filesystem=fat -p $senhaContainer --pim=420 -k "" --random-source=/dev/urandom
 
 # Monta o container
-#read -p "Insira o caminho do container que sera montado: " container
-veracrypt -t ./tmpVC
-echo "Lista dos containers montados: "
-veracrypt -t -l 
+veracrypt -t -p $senhaContainer -k "" --pim=420 --protect-hidden=no ./secureFileTransfer /media/veracrypt1/
 
 # Copia o arquivo para a unidade montada
-read -p "Qual arquivo você deseja criptografar e enviar? " arquivoUp
-#read -p "Qual o caminho do container montado? " caminhoUp
+read -p "Qual/quais arquivo/s você deseja criptografar e enviar? " arquivoUp
 cp $arquivoUp /media/veracrypt1/
 ls /media/veracrypt1/
 
 # Desmonta o container
-echo "Demontando o container para enviar..."
+echo "Desmontando o container para enviar..."
 veracrypt -t -d 
+
+# Adiciona segunda camada de protação com zip antes de enviar
+read -s -p "Defina a senha para o zip: " senhaZip
+zip secureFileTransfer.zip ./secureFileTransfer -P $senhaZip
 
 # Cria um wormhole com o container
 echo "Criando o wormhole com o container..."
-wormhole send ./tmpVC
+wormhole send ./secureFileTransfer.zip
+rm ./secureFileTransfer* 
+
+# ToDo
+## Fazer if questionando se deseja adicionar a segunda camada de proteção
+## Limpar saídas de erro do Veracrypt 
